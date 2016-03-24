59c4206a3a98439b9629e577e69d28a0288d4640
sutefun <su_te_fun@yahoo.co.id>
2016-03-24 18:11:33 +0700

    phase3

--- Account.java
+++ Account.java
@@ -1,14 +1,17 @@
 
 /**
  * @author (steven susanto) 
- * @version (5/3/16)
+ * @version (24 Maret 2016)
  */
 public  abstract class Account
 {
     protected double balance;
     protected String ID;
-
-       /** @return true,false    , true bila jumlah uang yang dideposit positif*/
+    
+       /** 
+        * @return true,false , true bila jumlah uang yang deposit positif*
+        * 
+        */
     public boolean deposit(double amount)
     {
         if(amount < 0)
@@ -24,37 +27,36 @@ public  abstract class Account
     }
     
     
-    /**@return balance  mengembalikan jumlah saldo  */
+    /**
+     * @return double:balance  ,mengembalikan jumlah saldo
+     */
     public double getBalance()
     {
         return balance;     
     }
     
-    /** @return ID  mengembalikan ID    */
+    /** 
+     * @return String:ID, mengembalikan ID  dari akun  
+     */
     public String getID()
     {
         return ID;      
     }
     
-    /**@param amount    mengatur balance*/
+    /**
+     * @param amount , mengatur balance
+     */
     public void setBalance(double amount)
     {
         balance=amount;     
     }
   
-    /**@return true,false   , true bila jumlah uang yang ditarik tIDak menyebabkan balance menjadi negatir  */
+    
+    /**@return true,false   , true bila jumlah uang yang ditarik tidak 
+     * menyebabkan balance menjadi negatir  
+     */
     public abstract boolean withdraw(double amount);
-    /*{
-        if(amount>=0&&amount<=balance)
-        {
-            balance-=amount;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }*/
+    
     
     /**
      * @return String:ID , merupakan override dari toString yang default terdapat pada setiap objek (dalam hal ini acocunt)
